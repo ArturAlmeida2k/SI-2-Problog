e3bf49eb34ea5106a08f024854de8700e9848581
P-Ramos16 <p.ramos@ua.pt>
2025-03-12 09:30:05 +0000

    Added demo n6

--- demo.py
+++ demo.py
@@ -77,6 +77,21 @@ evidence(constraint).
 evidence(excess(10),false).
 query(pack(_))."""
 
+    elif inp == 6:
+        string = """:- use_module(library(apply)).
+:- use_module(library(lists)).
+
+PH::make_coin(C,PH).
+coin(C) :- make_coin(C,0.8).
+tonum(C, Num) :- (coin(C), Num=1; \+coin(C), Num=0).
+
+total(S) :-
+    findall(X, between(1,10,X), L),
+    maplist(tonum, L, Nums),
+    sum_list(Nums, S).
+
+query(total(_))."""
+
     print("\033[F", end="")
     print(string)
     input()
